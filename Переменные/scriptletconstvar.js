31d385a1aecaf6b12e4a59b762aa64712ee146d1
Anya Krivosheeva <anyakrivosheeva98@gmail.com>
2025-02-12 20:01:12 +0300

    Домашка по переменным JS

---
+++ Переменные/scriptletconstvar.js
@@ -0,0 +1,24 @@
+let price = 100;
+price = 150;
+
+console.log(price);  // В консоли увидим значение: 150 , тк мы присвоили новое значение переменной price, объявленной с помощью ключевого слова let.
+
+
+const price2 = 200;
+price2 = 250;
+
+console.log(price2);  /*В консоли мы увидим ошибку: Uncaught TypeError: Assignment to constant variable.Так как мы не можем переопределять 
+                        значения переменных, записанных с помощью const.*/
+
+
+
+
+let x = 1;
+let x = 2;      // Что мы увидим в консоли:  Uncaught SyntaxError: Identifier 'x' has already been declared
+console.log(x);
+
+
+const y = 3;
+const y = 4;    // Что мы увидим в консоли:  Uncaught SyntaxError: Identifier 'y' has already been declared
+
+// Ошибка происходит из-за того, что невозможно повторно объявить переменную, объявленную с помощью let и const, в одной и той же области видимости.
